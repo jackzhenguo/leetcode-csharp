552fd2317880a8e86c6a8988abbfff4bff6a9f64
jack <guozhennianhua@163.com>
2017-04-21 08:30:27 +0800

    Array tag adds and three questions done

---
+++ Array/Array.Console/Array.Lib/PascalsTriangle.cs
@@ -0,0 +1,51 @@
+﻿/* ==============================================================================
+ * 功能描述：PascalsTriangle  
+ * 创 建 者：wack
+ * 创建日期：2017/4/21 8:22:58
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Given numRows, generate the first numRows of Pascal’s triangle.
+
+//For example, given numRows = 5, 
+//Return
+
+//[
+//     [1],
+//    [1,1],
+//   [1,2,1],
+//  [1,3,3,1],
+// [1,4,6,4,1]
+//]
+namespace Array.Lib
+{
+    /// <summary>
+    /// #118: Pascal’s Triangle
+    /// </summary>
+    public class PascalsTriangle
+    {
+
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> rtn = new List<IList<int>>();
+            for (int i = 0; i < numRows; i++)
+            {
+                IList<int> ints = new List<int>();
+                ints.Add(1);
+                for (int j = 1; j < i; j++)
+                {
+                    IList<int> intsPreRow = rtn[i - 1];
+                    ints.Add(intsPreRow[j - 1] + intsPreRow[j]);
+                }
+                if (i > 0)
+                    ints.Add(1);
+
+                rtn.Add(ints);
+            }
+            return rtn;
+        }
+
+    }
+}
---
+++ Array/Array.Console/Array.Lib/SearchInsertPosition.cs
@@ -0,0 +1,46 @@
+﻿/* ==============================================================================
+ * 功能描述：SearchInsertPosition  
+ * 创 建 者：gz
+ * 创建日期：2017/4/21 8:21:07
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+//You may assume no duplicates in the array.
+
+//Here are few examples. 
+//[1,3,5,6], 5 → 2 
+//[1,3,5,6], 2 → 1 
+//[1,3,5,6], 7 → 4 
+//[1,3,5,6], 0 → 0
+
+namespace Array.Lib
+{
+    /// <summary>
+    /// #35: Search Insert Position
+    /// </summary>
+    public class SearchInsertPosition
+    {
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int lo = 0;
+            int hi = nums.Length;
+            while (lo < hi)
+            {
+                int mi = (lo + hi) >> 1;
+                if (target <= nums[mi]) //目标值不大于中间位置的数时，hi变小
+                    hi = mi;
+                else if (target > nums[mi]) //大于中间位置的值，lo加1
+                    lo = lo + 1;
+            }
+            return lo;
+        }
+
+
+    }
+}
