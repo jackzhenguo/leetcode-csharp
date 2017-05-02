dd7a332c9a24c04902f22df3104be66426f2f25c
jackz <guozhennianhua@163.com>
2017-05-02 22:04:01 +0800

    remove element

---
+++ Array/Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using Array.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveElementSln sln = new RemoveElementSln();
+            int cnt = sln.RemoveElement(new int[] { 3, 2, 2, 3 }, 3);
+        }
+    }
+}
