68dac901980e19387e55590324f677799dbd5515
jackz <guozhennianhua@163.com>
2017-05-06 18:31:02 +0800

    array partitionI and reshape matrix

---
+++ Array/Array.Lib/ArrayPartitionISln.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array.Lib
+{
+    public class ArrayPartitionISln
+    {
+        //        Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), …, (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+        //Example 1:
+        //Input: [1,4,3,2]
+
+        //Output: 4
+        //Explanation: n is 2, and the maximum sum of pairs is 4.
+        public int ArrayPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int rtn = 0;
+            for (int i = 0; i < nums.Length; i = i + 2)
+                rtn += nums[i];
+            return rtn;
+        }
+    }
+}
---
+++ Array/Array.Lib/ReshapeMatrixSln.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array.Lib
+{
+    //    In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+    //You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+    //The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+    //If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+    //Example 1:
+    //Input: 
+    //nums = 
+    //[[1,2],
+    // [3,4]]
+    //r = 1, c = 4
+    //Output: 
+    //[[1,2,3,4]]
+    //Explanation:
+    //The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+    //Example 2:
+    //Input: 
+    //nums = 
+    //[[1,2],
+    // [3,4]]
+    //r = 2, c = 4
+    //Output: 
+    //[[1,2],
+    // [3,4]]
+    //Explanation:
+    //There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+    public class ReshapeMatrixSln
+    {
+        // i*c+j = si*dim1+sj;
+        public int[,] MatrixReshape(int[,] nums, int r, int c)
+        {
+            int dim0 = nums.GetUpperBound(0);
+            int dim1 = nums.GetUpperBound(1);
+            int si = 0, sj = 0;
+            if (r * c != (dim0+1) * (dim1+1)) return nums;
+            int[,] rtn = new int[r, c];
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++,sj++){
+                    if (sj > dim1){
+                        si++;
+                        sj = 0;
+                    }
+                    rtn[i, j] = nums[si, sj];
+                }
+             }
+            return rtn;
+        }
+    }
+}
