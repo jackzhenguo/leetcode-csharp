e41ff0548c462595fb44537fabceb6d4641b4dc6
jackz <guozhennianhua@163.com>
2017-05-07 20:01:06 +0800

    dictionary application

---
+++ HashTable/HashTable.Lib/IsomorphicStringsSln.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HashTable.Lib
+{
+    public class IsomorphicStringsSln
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var dict = new Dictionary<char, char>(); //s.char->t.char
+            var dict2 = new Dictionary<char, char>(); //t.char->s.char
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]) && dict[s[i]] != t[i] ||
+                   dict2.ContainsKey(t[i]) && dict2[t[i]] != s[i])
+                    return false;
+                dict[s[i]] = t[i];
+                dict2[t[i]] = s[i];
+            }
+            return true;
+        }
+    }
+}
---
+++ HashTable/HashTable.Lib/WordPattenSln.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HashTable.Lib
+{
+    public class WordPattenSln
+    {
+        //pattern = "abba", str = "dog cat cat dog"
+        public bool WordPattern(string pattern, string str){
+            string[] words = str.Split(' ');
+            if (pattern.Length != words.Length) return false;
+            Dictionary<char, string> patternDict = new Dictionary<char, string>();
+            Dictionary<string, char> wordsDict = new Dictionary<string, char>();
+            for (int i = 0; i < pattern.Length; i++){
+                if(patternDict.ContainsKey(pattern[i]) && patternDict[pattern[i]]!=words[i] ||
+                   wordsDict.ContainsKey(words[i]) && wordsDict[words[i]]!=pattern[i] )
+                    return false;
+                patternDict[pattern[i]] = words[i];
+                wordsDict[words[i]] = pattern[i];
+            }
+            return true;
+        }
+    }
+}
