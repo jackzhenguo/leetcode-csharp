1523d1ca2ec71006d2ce932c06c9b8f8e0024b0c
jack <guozhennianhua@163.com>
2017-05-09 13:34:04 +0800

    FindDifferenceSln

---
+++ HashTable/HashTable.Lib/FindDifferenceSln.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述：FindDifferenceSln  
+ * 创 建 者：gz
+ * 创建日期：2017/5/9 13:23:46
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HashTable.Lib
+{
+    /// <summary>
+    /// FindDifferenceSln
+    /// </summary>
+    public class FindDifferenceSln
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int[] hash = new int[123]; //97~122
+            foreach (var ch in s)
+                hash[Convert.ToInt32(ch)]++;
+            foreach (var ch in t)
+            {
+                int tmp = Convert.ToInt32(ch);
+                hash[tmp]--;
+                if (hash[tmp] < 0)
+                    return ch;
+            }
+            return ' ';
+        }
+
+    }
+}
