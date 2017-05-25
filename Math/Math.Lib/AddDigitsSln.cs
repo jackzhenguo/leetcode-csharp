3ec0b58e997c1cf30b09ccef214157c7797fdce7
jack <guozhennianhua@163.com>
2017-05-25 13:30:28 +0800

    add digits

---
+++ Math/Math.Lib/AddDigitsSln.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// // Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// // For example:
+
+// // Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+// // Follow up: 
+// // Could you do it without any loop/recursion in O(1) runtime?
+namespace Math.Lib
+{
+    public class AddDigitsSln
+    {
+        public int AddDigits(int num) {
+        return num - 9*Convert.ToInt32((num-1)/9);
+    }
+    }
+
+}
---
+++ Math/Math.Lib/PalindromeNumberSln.cs
@@ -0,0 +1,38 @@
+﻿/* ==============================================================================
+ * 功能描述：PalindromeNumberSln  
+ * 创 建 者：gz
+ * 创建日期：2017/5/24 13:01:57
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math.Lib
+{
+    /// <summary>
+    /// PalindromeNumberSln
+    /// </summary>
+    public class PalindromeNumberSln
+    {
+        public bool IsPalindrome(int x)
+        {
+            int palindromex = 0, tmp = x;
+            int sumbit = 0;
+            //calcuate bit count
+            while (tmp > 0)
+            {
+                sumbit++;
+                tmp /= 10;
+            }
+            tmp = x;
+            while (tmp > 0)
+            {
+                palindromex += tmp % 10 * (int)System.Math.Pow(10, --sumbit);
+                tmp /= 10;
+            }
+            return palindromex == x;
+        }
+
+    }
+}
